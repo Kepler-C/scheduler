e5e87531d31a1d1c7d35fd1896ce0c42b31feb0c
Kallahan <kallahan@eshop-guide.de>
2022-08-24 22:29:52 +0200

    add delete and edit actions

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,5 +7,42 @@ class AppointmentsController < ApplicationController
   def show 
     @appointment = Appointment.find(params[:id]) 
   end
+
+  def new 
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to @appointment
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update(appointment_params)
+      redirect_to @appointment
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def appointment_params
+      params.require(:appointment).permit(:patient_name, :date, :time, :issue)
+    end
   
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,6 @@
 class Appointment < ApplicationRecord
+  validates :patient_name, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :issue, presence: true, length: { minimum: 10 }
 end
